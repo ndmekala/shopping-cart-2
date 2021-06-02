7b778c333845e1f507f7c1341ab9112e005c60a6
Nirmal Mekala <nirmalmekala@Nirmals-MacBook-Air.local>
2021-06-01 22:25:29 -0500

    building out data processing

--- src/App.js
+++ src/App.js
@@ -12,19 +12,44 @@ const App = () => {
 // 	.then(json => console.log(json))
 // 	.catch(error => console.log(error))
 	const pullItems = async function() {
+		//needs error handling
 		const response = await fetch('https://shielded-peak-43727.herokuapp.com/etsy/shops/6127899/listings/active')
-		// if error… try / catch…
 		const items = await response.json();
-		return items
+		console.log(items)
+		return items;
+	}
+	
+	const pullItemImages = async function (id) {
+		// needs some error handling
+		const response = await fetch(`https://shielded-peak-43727.herokuapp.com/etsy/listings/${id}/images`)
+		const obj = await response.json();
+		console.log(obj)
+		return obj;
+	}
+	
+	const findImageObj = function (id, array) {
+		let arr = array.filter(element => Number(element.params.listing_id) === Number(id))
+		console.log(arr)
+		return arr
+	}
+	
+	const getAllData = async function() {
+		const allItems = await pullItems();
+		let allImages = [];
+		for (const result of allItems.results) {
+			const imageSet = await pullItemImages(result.listing_id)
+			await allImages.push(imageSet)
+			console.log(allImages)
+		}
+		await setImages(allImages)
+		await setItems(allItems)
 	}
 	
 	const [items, setItems] = useState({results: []});
+	const [images, setImages] = useState([]);
 	
 	useEffect(() => {
-		pullItems().then(response => {
-			setItems(response);
-			console.log(response);
-			})	
+		getAllData()
 	}, []);
 	
 	return (
@@ -38,6 +63,7 @@ const App = () => {
 					<ul>
 						<li>{result.description}</li>
 						<li>${result.price}</li>
+						{/*<li>{findImageObj(result.listing_id, images).results[0].listing_image_id}</li>*/}
 					</ul>
 				</li>
 			))}
