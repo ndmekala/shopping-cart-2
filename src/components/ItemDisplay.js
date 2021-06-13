22c7b22a7dc557c2e69bb57261479264cc63e53f
Nirmal Mekala <ndmekala@icloud.com>
2021-06-12 20:30:22 -0500

    got props working

--- src/components/ItemDisplay.js
+++ src/components/ItemDisplay.js
@@ -6,46 +6,24 @@ const ItemDisplay = (props) => {
 	let { listingID } = useParams();
 	console.log('hi')
 	console.table(props)
+	let itemData
 
-	// const findData = function () {
-		// props.itemDataset.forEach((page) => )
-	// }
-
-	const data = function() {
+	const findData = function() {
 		let arr = props.itemDataset.filter(element => element.id == listingID)
-		return arr
+		itemData = arr[0]
 	}
-
-	console.table(data())
-
-
-
-
-	// const findData = function () {
-	// 	let arr = props.itemDataset.filter(
-	// 	  (element) => Number(element.params.listing_id) === Number(listingID)
-	// 	);
-	// 	return arr[0];
-	//   };
+	findData();
+	console.table(itemData)
 	
 	return (
 		<Container >
-	<h1>Hi from ItemDisplay</h1>
-
-	{/* <pre>{JSON.stringify(findImageObj(), null, 2)}</pre> */}
-	{/* {findImageObj().results.map((imgResult) => (<img */}
-	{/* key = {imgResult.listing_image_id} */}
-	{/* src = {imgResult.url_570xN} />))} */}
-	{/* <h2>{props.itemDataset[]}</h2> */}
-	<h2>{listingID}</h2>
-	{/* <p>{props.itemDataset[0][7].id}</p> */}
-	<pre>{JSON.stringify(data(), null, 2)}</pre>
-	{/* <pre>{JSON.stringify(props.imageData[0].params.listing_id, null, 2)}</pre> */}
-	{/* <pre>{JSON.stringify(props.imageData, null, 2)}</pre> */}
-	<pre>{JSON.stringify(listingID), null, 2}</pre>
-	{/* {findImageObj.results.map((result) => (
-		<img src={result.url_75x75} />
-	))} */}
+	<h1>{itemData.title}</h1>
+	{itemData.imageData.results.map((imgResult) => (
+		<img src={imgResult.url_570xN}/>
+		
+	))}
+	<p>{itemData.description}</p>
+	<p>{itemData.price}</p>
 		</Container>
 	
 	);
