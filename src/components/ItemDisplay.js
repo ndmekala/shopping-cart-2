954758b684b968d155d30830c42c61358f23df5b
Nirmal Mekala <ndmekala@icloud.com>
2021-06-20 23:29:36 -0500

    building bootstrap sections

--- src/components/ItemDisplay.js
+++ src/components/ItemDisplay.js
@@ -1,44 +1,53 @@
 import React, { useState, useEffect } from "react"
 import { useParams } from 'react-router-dom';
 import Container from "react-bootstrap/Container";
-import Carousel from "react-bootstrap/Carousel";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button"
 
 const ItemDisplay = (props) => {
-	let { listingID } = useParams();
-	console.log('hi')
-	console.table(props)
-	let itemData
+  let { listingID } = useParams();
+  console.log('hi')
+  console.table(props)
+  let itemData
 
-	const textWrapperStyles = {
-		marginTop: '4rem',
-		marginBottom: '2rem',
-	}
+  const [imageNumber, setImageNumber] = useState(0)
 
-	const findData = function() {
-		let arr = props.itemDataset.filter(element => element.id == listingID)
-		itemData = arr[0]
-	}
-	findData();
-	console.table(itemData)
-	
-	return (
-    <Container>
-      <h1 dangerouslySetInnerHTML={{ __html: itemData.title }} />
-      <Carousel>
-        {itemData.imageData.results.map((imgResult) => (
-          <Carousel.Item>
-            <img className="d-block w-100" src={imgResult.url_570xN} />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+  const findData = function () {
+    let arr = props.itemDataset.filter(element => element.id == listingID)
+    itemData = arr[0]
+  }
+  findData();
+  console.table(itemData)
+
+  return (
+    <Container style={{marginTop: '1rem'}}>
       <Row>
-        <Col style={textWrapperStyles} xs={{span: 12}} md={{span: 8, offset: 2}}>
-          <p dangerouslySetInnerHTML={{ __html: itemData.description }} />
-          <p>${itemData.price}</p>
-		  <Button id={listingID} onClick={props.addToCart}>Add to Cart</Button>
+        <Col>
+          <Row>
+            <Col >
+              <img src={itemData.imageData.results[imageNumber].url_570xN}></img>
+            </Col>
+          </Row>
+          <Row style={{marginTop: '1rem'}}>
+            {itemData.imageData.results.map((imgResult) => (
+              <Col xs={4} sm={3} md={2} lg={2}>
+                <img className="d-block w-100"
+                  src={imgResult.url_570xN}
+                  onClick={() => { setImageNumber(itemData.imageData.results.indexOf(imgResult)) }} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+        <Col>
+          <Row>
+            <Col>
+              <h1 className="item-display-header" dangerouslySetInnerHTML={{ __html: itemData.title }} />
+              <Button style={{marginBottom: '1rem'}} id={listingID} onClick={props.addToCart}>Add to Cart</Button>
+              <p>${itemData.price}</p>
+              <p dangerouslySetInnerHTML={{ __html: itemData.description }} />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const Welcome = () => {
 		return (
       <div>
         <Jumbotron className="welcome-hero" fluid>
         </Jumbotron>
+        <Container>
+          <Row>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/Road_Runner_Bags_Shop_USA_Made_1.jpg?v=1591294701" />
+            </Col>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/RRB_sold_here_grande.jpg?v=1543846166" />
+            </Col>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/Road_Runner_Bags_Postal_Run.jpg?v=1608224911" />
+            </Col>
+          </Row>
+        </Container>
       </div>
       
     );
