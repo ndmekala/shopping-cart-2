d2800fa8952493be8dfa0fd0d44235b7a9cc0705
Nirmal Mekala <ndmekala@icloud.com>
2021-06-20 23:32:29 -0500

    building out landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -21,6 +21,17 @@ const Welcome = () => {
             <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/Road_Runner_Bags_Postal_Run.jpg?v=1608224911" />
             </Col>
           </Row>
+          <Row>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/Road_Runner_Bags_Shop_USA_Made_1.jpg?v=1591294701" />
+            </Col>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/RRB_sold_here_grande.jpg?v=1543846166" />
+            </Col>
+            <Col lg={4}>
+            <img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/1384/6395/files/Road_Runner_Bags_Postal_Run.jpg?v=1608224911" />
+            </Col>
+          </Row>
         </Container>
       </div>
       
