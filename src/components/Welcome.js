7ccc94ee6f574fdd05370ff0277d99e83b48609a
Nirmal Mekala <ndmekala@icloud.com>
2021-06-21 21:20:03 -0500

    removing unneccesary import

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,6 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button'
 
 const CardLink = (props) => {
 
